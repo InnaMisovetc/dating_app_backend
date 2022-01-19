5e7ed438e935144bc2f385a837a02df24640381e
Inna Misovetc <inna.misovets@skoltech.ru>
2022-01-20 01:35:20 +0300

    Add files for deployment to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dating_app_backend.wsgi:application --log-file -
--- dating_app_backend/settings.py
+++ dating_app_backend/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
+import os.path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,6 +141,11 @@ REST_FRAMEWORK = {
     )
 }
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=3600),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 ***REMOVED***
@@ -147,3 +154,6 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 ***REMOVED***
 ***REMOVED***
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+django_heroku.settings(locals())
